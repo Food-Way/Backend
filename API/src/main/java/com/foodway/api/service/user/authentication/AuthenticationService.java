3920667fcae9e44bd7ceae4b611f23e85fe13b7c
jofsan <jonatas.santos@sptech.school>
2023-09-20 22:50:43 -0300

    security implement

--- API/src/main/java/com/foodway/api/service/user/authentication/AuthenticationService.java
+++ API/src/main/java/com/foodway/api/service/user/authentication/AuthenticationService.java
@@ -4,9 +4,11 @@ import com.foodway.api.model.User;
 import com.foodway.api.repository.UserRepository;
 import com.foodway.api.service.user.authentication.dto.UserDetailsDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,6 +16,7 @@ import java.util.Optional;
 public class AuthenticationService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Optional<User> userOptional = userRepository.findByEmail(email);
@@ -21,6 +24,18 @@ public class AuthenticationService implements UserDetailsService {
         if(userOptional.isEmpty()){
             throw  new UsernameNotFoundException(String.format("User %s not found", email));
         }
-        return new UserDetailsDto(userOptional.get());
+        User user = userOptional.get();
+        String passwordData = user.getPassword();
+
+        // Verifique a senha usando o BCryptPasswordEncoder
+
+        boolean wrongPassword = passwordEncoder.matches("password123", passwordData);
+
+        if (!wrongPassword) {
+            throw new BadCredentialsException("Senha incorreta");
+        }
+
+        return new UserDetailsDto(user);
+
     }
 }
