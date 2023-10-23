c42b14ee7f15dd5a0ae7af61dd9f1ca6784f82ab
Fernandeess <matheus.rodrigues@sptech.school>
2023-10-23 13:53:43 -0300

    Fixing: retorno do id login

--- API/src/main/java/com/foodway/api/controller/UserController.java
+++ API/src/main/java/com/foodway/api/controller/UserController.java
@@ -13,6 +13,9 @@ import com.foodway.api.service.UserService;
 import com.foodway.api.service.user.authentication.dto.UserCreateDto;
 import com.foodway.api.service.user.authentication.dto.UserLoginDto;
 import com.foodway.api.service.user.authentication.dto.UserTokenDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,7 +53,14 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<Void> get() {
+    @Operation(summary = "Check if user is authenticated", method = "HTTP GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User is authenticated"),
+            @ApiResponse(responseCode = "403", description = "User is not authenticated"),
+            @ApiResponse(responseCode = "500", description = "An unexpected error occurred while processing the request")
+    })
+    public ResponseEntity  get() {
+        // Se o usuário está autenticado, retorna um código de status 200
         return ResponseEntity.ok().build();
     }
 
