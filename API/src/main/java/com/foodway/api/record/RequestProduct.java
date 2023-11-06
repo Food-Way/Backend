54135ff1e2f5a3bb4f5ec4115611f54eef741090
MarceloACJunior <marcelo.cruzjr@sptech.school>
2023-11-06 18:29:10 -0300

    nsei

--- API/src/main/java/com/foodway/api/record/RequestProduct.java
+++ API/src/main/java/com/foodway/api/record/RequestProduct.java
@@ -7,9 +7,8 @@ import java.time.LocalDateTime;
 
 public record RequestProduct(
         @NotNull String name,
-        @NotNull String description,
+        String description,
         @NotNull BigDecimal price,
-        @NotNull LocalDateTime updatedAt,
-        @NotNull String photo
+        String photo
 ) {
 }
