abf1b5024198db32453dd4e93a21d5a62899fbd4
MarceloACJunior <marcelo.cruzjr@sptech.school>
2023-11-27 20:53:49 -0300

    adicionada images default

--- API/src/main/java/com/foodway/api/model/User.java
+++ API/src/main/java/com/foodway/api/model/User.java
@@ -54,8 +54,8 @@ public abstract class User {
         this.email = email;
         this.password = encodePassword(password);
         this.typeUser = typeUser;
-        this.profilePhoto = profilePhoto;
-        this.profileHeaderImg = profileHeaderImg;
+        this.profilePhoto = profilePhoto == null || profilePhoto.isEmpty() ? "https://foodway.blob.core.windows.net/public/default-user-image.png" : profilePhoto;
+        this.profileHeaderImg = profileHeaderImg == null || profileHeaderImg.isEmpty() ? "https://foodway.blob.core.windows.net/public/default-banner.png" : profileHeaderImg;
         this.culinary = culinary;
     }
 
