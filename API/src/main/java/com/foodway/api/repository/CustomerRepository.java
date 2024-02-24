173301880e1f69ab79f6e494138e2597b2a96821
MarceloSPTECH <marcelo.cruzjr@sptech.school>
2024-02-24 16:51:43 -0300

    added toggleCommentUpvote method

--- API/src/main/java/com/foodway/api/repository/CustomerRepository.java
+++ API/src/main/java/com/foodway/api/repository/CustomerRepository.java
@@ -11,7 +11,4 @@ public interface CustomerRepository extends JpaRepository<Customer,UUID> {
     Customer findByIdUserAndPassword(UUID idUser, String password);
     Customer findByEmailAndPassword(String email, String password);
     List<Customer> findByNameContainsIgnoreCase(String name);
-
-    long countByUpvoteList_IdCustomer(UUID idCustomer);
-
 }
