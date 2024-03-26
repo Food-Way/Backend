1e2c042a83a470161da029e9c293330c1b607d79
MarceloSPTECH <marcelo.cruzjr@sptech.school>
2024-03-26 00:33:35 -0300

    arrumado o smtp

--- API/src/main/java/com/foodway/api/apiclient/SimpleMailClient.java
+++ API/src/main/java/com/foodway/api/apiclient/SimpleMailClient.java
@@ -1,11 +1,10 @@
 package com.foodway.api.apiclient;
 
 import com.foodway.api.apiclient.entities.SimpleMail;
-import com.foodway.api.apiclient.entities.SimpleMailAccountCreated;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "simpleMailClient", url = "http://localhost:80")
+@FeignClient(name = "simpleMailClient", url = "http://54.90.124.165:80")
 public interface SimpleMailClient {
 
     @PostMapping("{uri}")
