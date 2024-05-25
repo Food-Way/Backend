b752316fa329fefd45a41d0159f89e6ee1b8ec67
MarceloACJunior <marcelinhoalcantaracruz@gmail.com>
2024-05-25 13:16:55 -0300

    fix: adjusting jacoco configs

---
+++ API/src/test/java/com/foodway/api/ApiApplicationTest.java
@@ -0,0 +1,12 @@
+package com.foodway.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
