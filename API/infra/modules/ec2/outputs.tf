136cbc9ff6f88d15d104c4376bac7da203cbb4fb
leobr1t0 <leonardo.oliveira@sptech.school>
2024-05-30 20:10:20 -0300

    test ci/cd

--- API/infra/modules/ec2/outputs.tf
+++ API/infra/modules/ec2/outputs.tf
@@ -1,7 +1,7 @@
-output "ec2_01_private_ip" {
+output "private_ec2_01" {
   value = aws_instance.private_ec2_01.private_ip
 }
 
-output "ec2_02_private_ip" {
+output "private_ec2_02" {
   value = aws_instance.private_ec2_02.private_ip
 }
