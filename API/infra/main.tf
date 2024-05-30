4a93278f2e5999be26650236a67cf0990966b227
leobr1t0 <leonardo.oliveira@sptech.school>
2024-05-30 18:09:47 -0300

    test ci/cd

--- API/infra/main.tf
+++ API/infra/main.tf
@@ -9,7 +9,9 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 module "ec2" {
--- API/infra/variables.tf
+++ API/infra/variables.tf
@@ -3,3 +3,13 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
