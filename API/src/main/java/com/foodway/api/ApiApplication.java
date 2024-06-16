43ad0a2228bf4d44ad0744acce5519e782e3d373
fernandeeess <matheus.rodrigues@sptech.school>
2024-06-16 14:13:24 -0300

    correção dependencias

--- API/src/main/java/com/foodway/api/ApiApplication.java
+++ API/src/main/java/com/foodway/api/ApiApplication.java
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "FoodWay API", version = "1.0", description = "Documentation FoodWay API v1.0"), servers = {@Server(url = "http://localhost:5225/api", description = "Base URL API FoodWay")})
 @SecurityRequirement(name = "Bearer Authentication")
+@EnableFeignClients
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
